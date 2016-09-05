c43c31a1cbbc4696134fe4fc243b1c0ec9f7d7a9
F-Fischer <francisco.fischer92@gmail.com>
2016-09-05 14:53:59 -0300

    Cambios en Articulos.cs

--- InformaticaIndustrial/Vista/Articulos.cs
+++ InformaticaIndustrial/Vista/Articulos.cs
@@ -40,7 +40,7 @@ namespace InformaticaIndustrial.Vista
         private void lbArticulos_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             int index = this.lbArticulos.IndexFromPoint(e.Location);
-            System.Collections.IList list = aDAO.getArticuloById(index);
+            System.Collections.IList list = aDAO.getArticuloById(index + 1);
             System.Collections.IList listDesc = dDAO.getDescripcionById(((articulo)(list[0])).descripcion_id);
             System.Collections.IList listTipoArticulo = taDAO.getTipoArticulosById(((articulo)(list[0])).tipo_articulo);
             //System.Collections.IList listUnidadMedida = umDAO.getUnidadMedidaById(((articulo)(list[0])).unidad_med);
@@ -54,7 +54,7 @@ namespace InformaticaIndustrial.Vista
             txtPre.Text = (((articulo)(list[0])).precio_std).ToString();
             txtDescripcion.Text = ((descripcion)(listDesc[0])).descripcion_str;
             txtTipoArticulo.Text = listTipoArticulo[0].ToString();
-            txtUnidadMedida.Text = listUnidadMedida[0].ToString();
+            //txtUnidadMedida.Text = listUnidadMedida[0].ToString();
 
 
         }
